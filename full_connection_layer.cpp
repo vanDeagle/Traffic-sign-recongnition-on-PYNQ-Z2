ba47c1f7a6c439220fbbf5cc6ee9166d43d3f4f5
kyle <13136177656@163.com>
2020-07-28 13:57:43 +0800

    test_pass, waiting for changing the interface port

--- full_connection_layer.cpp
+++ full_connection_layer.cpp
@@ -1,3 +1,16 @@
+/***********************************************
+Author: Kyle 
+Associated Filename: full_connection.cpp
+Purpose:to acceleration of the full_connection_layer
+Device: All 
+Revision History: July 28,2020
+------------------------------------------
+Meanings of registers:
+input_data       ---->   DDR address that point to input data
+output_data      ---->   DDR address that point to result data
+weights          ---->   DDR address that point to weights data
+***********************************************/
+
 #include "full_connection_layer.h"
 
 void full_connection_layer1(
@@ -7,23 +20,32 @@ void full_connection_layer1(
     bool active   
 )
 {
+#pragma HLS INTERFACE s_axilite port=return
+#pragma HLS INTERFACE m_axi depth=30 port=weights
+#pragma HLS INTERFACE m_axi depth=30 port=output_data
+#pragma HLS INTERFACE m_axi depth=30 port=input_data
     float input_data_buf[INPUT_NUM1];
+#pragma HLS ARRAY_PARTITION variable=input_data_buf complete dim=1
     float output_data_buf[OUTPUT_NUM1];
+#pragma HLS ARRAY_PARTITION variable=output_data_buf complete dim=1
     float weights_buf[OUTPUT_NUM1][INPUT_NUM1];
+#pragma HLS ARRAY_PARTITION variable=weights_buf complete dim=2
 
     load_data:
     {
-        for (int i = 0; i < INPUT_NUM1; i++)
+        load_input:for (int i = 0; i < INPUT_NUM1; i++)
         {
+#pragma HLS PIPELINE
             /* code */
             input_data_buf[i] = *input_data++;
         }
 
-        for (int i = 0; i < OUTPUT_NUM1; i++)
+        load_weights:for (int i = 0; i < OUTPUT_NUM1; i++)
         {
             /* code */
             for (int j = 0; j < INPUT_NUM1; j++)
             {
+#pragma HLS PIPELINE
                 /* code */
                 weights_buf[i][j] = *weights++;
             }    
@@ -32,16 +54,34 @@ void full_connection_layer1(
 
     full_C_cal:
     {
-        OUT_NUM:for (int i = 0; i < OUTPUT_NUM1; i++)
+    	calculate:for (int i = 0; i < OUTPUT_NUM1; i++)
         {
+#pragma HLS PIPELINE
             /* code */
+            float temp = 0;
             PROCESS:for (int j = 0; j < INPUT_NUM1; j++)
             {
                 /* code */
-                output_data_buf[i] += input_data_buf[j] * weights_buf[i][j];
+                temp += input_data_buf[j] * weights_buf[i][j];
             }
             if(active)
-                output_data_buf[i] = (output_data_buf[i] > 0) ? output_data_buf[i] : 0;
+                output_data_buf[i] = (temp > 0) ? temp : 0;
+            else
+            {
+                output_data_buf[i] = temp;
+            }
+            
         }
+    }
+
+    output_overload:
+    {
+        read_back:for (int i = 0; i < OUTPUT_NUM1; i++)
+        {
+#pragma HLS PIPELINE
+            /* code */
+            *output_data++ = output_data_buf[i]; 
+        }
+        
     }
 }
